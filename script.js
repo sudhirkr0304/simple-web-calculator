659e509e0dd164872cbd47b873a4cfb1766045b5
sudhir kumar <37249975+sudhirkr0304@users.noreply.github.com>
2020-05-08 13:01:46 +0530

    all done

---
+++ script.js
@@ -0,0 +1,18 @@
+//alert("Hello");
+
+
+function tdclick(text)
+{
+    document.getElementById("value").value = document.getElementById("value").value + text;
+}
+
+
+function erase()
+{
+    document.getElementById("value").value = "";
+}
+
+function calculate()
+{
+    document.getElementById("value").value = eval(document.getElementById("value").value);
+}
